140996fdb7aa0f8148f3ec11fedc29f552acf4c6
James Kiesel <james.kiesel@gmail.com>
2015-08-07 15:50:06 -0700

    Fix snorlax_used_rest

--- lib/snorlax.rb
+++ lib/snorlax.rb
@@ -8,6 +8,6 @@ class ActionController::Base
   # Method to allow controllers to apply Snorlax to themselves
   # (g@nked from Inherited resources: https://github.com/josevalim/inherited_resources/blob/master/lib/inherited_resources.rb)
   def self.snorlax_used_rest!
-    RestfulController::Base.snorlax_used_rest!(self)
+    Snorlax::Base.snorlax_used_rest!(self)
   end
 end
